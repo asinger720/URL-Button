dcdbc6a4db247da5286d1b6591889b970e1781d3
asinger720 <47115598+asinger720@users.noreply.github.com>
2019-10-23 13:28:10 -0400

    Update URL Buttoncontroller.js

--- URL Buttoncontroller.js
+++ URL Buttoncontroller.js
@@ -1,18 +1,17 @@
 ({
-      //Open URL in New Browser Tab With Record Id
-    OpenURL : function(component, event, helper) {
+	//Open URL in New Browser Tab With Record Id
+	OpenURL : function(component, event, helper) {
         var recordId = component.get('v.recordId');
         var url = component.get('v.url');
         var open = component.get('v.open');
         var includeRecord = component.get('v.includeRecord');
         
-       
-        if(component.get('v.includeRecord') == true){
-             window.open(url + recordId, open);
-        }
-       else
-           window.open(url, open);
-  
-       
+        var url2Open = url;
+        if(includeRecord == true){ url2Open += recordId;}
+        
+        var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({ "url": url2Open });
+        urlEvent.fire();
+		//window.open(url2Open, open);  
     }
 })
